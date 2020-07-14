16033e2a60589250b029eac527419c51a4f68c34
yurizhang <yurizhang@sohu-inc.com>
2020-07-14 22:44:10 +0800

    新建ImageDo

---
+++ chl-resource-server-common/src/main/java/team/hulu/cms/pojo/ImageDO.java
@@ -0,0 +1,47 @@
+package team.hulu.cms.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author: yurizhang
+ * @date: 2020/7/14 9:22 下午
+ */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class ImageDO {
+//    /**
+//     * 自增主键
+//     */
+//    @Id
+//    private Integer id;
+    /**
+     * 图片名
+     */
+    private String name;
+    /**
+     * 创建用户
+     */
+    private String createUser;
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+    /**
+     * 更新用户
+     */
+    private String updateUser;
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+}
---
+++ chl-resource-server-web/src/test/java/team/hulu/cms/WebTest.java
@@ -0,0 +1,34 @@
+package team.hulu.cms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import team.hulu.cms.pojo.ImageDO;
+import team.hulu.cms.util.MongoUtils;
+
+/**
+ * @author: yurizhang
+ * @date: 2020/7/14 9:38 下午
+ */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class WebTest {
+
+    @Autowired
+    private MongoUtils mongoUtils;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void testMongo(){
+        ImageDO imageDO = new ImageDO();
+        imageDO.setName("test1");
+        mongoTemplate.insert(imageDO, "images");
+
+    }
+}
